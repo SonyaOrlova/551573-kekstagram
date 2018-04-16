572c4ac9822f373506e7d0d24eb49df9006c2321
SonyaOrlova <sonya5488@yandex.ru>
2018-04-16 13:38:52 +0300

    Fixes

--- js/pictures.js
+++ js/pictures.js
@@ -8,8 +8,8 @@ var random = function (min, max) {
 
 var createPictures = function (picturesNumber) {
   var pictures = [];
-  var descriptionArr = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-  var phrasesArr = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  var descriptions = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+  var phrases = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
   for (var i = 0; i <= picturesNumber - 1; i++) {
     pictures[i] = {
@@ -17,13 +17,13 @@ var createPictures = function (picturesNumber) {
       likes: random(15, 200),
       comments:
       (function () {
-        var commentsArr = [];
+        var comments = [];
         for (var j = 0; j < random(1, 2); j++) {
-          commentsArr[j] = phrasesArr [random(0, phrasesArr.length - 1)];
+          comments[j] = phrases[random(0, phrases.length - 1)];
         }
-        return commentsArr;
+        return comments;
       })(),
-      description: descriptionArr[random(0, descriptionArr.length - 1)]
+      description: descriptions[random(0, descriptions.length - 1)]
     };
   }
   return pictures;
@@ -77,7 +77,7 @@ var onEditorEscPress = function (evt) {
 
 // сбрасывает значения выбора
 var cleanSelect = function () {
-  selectFile.value = ('');
+  selectFile.value = '';
 
   // *сброс размеров
   currentSize = defaultSize;
@@ -365,10 +365,13 @@ var renderBigPhoto = function (photo) {
 };
 
 // открытие фото (событие)
+
+var onPhotoClick = function (evt) {
+  renderBigPhoto(evt.target.parentNode)
+};
+
 for (i = 0; i < photos.length; i++) {
-  photos[i].addEventListener('click', function (evt) {
-    renderBigPhoto(evt.target.parentNode);
-  });
+  photos[i].addEventListener('click', onPhotoClick);
 }
 
 // закрытие фото (событие)
